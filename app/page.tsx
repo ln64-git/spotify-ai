0b8e32ccfb494769a3ff7314e3511bb91f612a4d
Luke Anthony <ln64.ohio@gmail.com>
2025-04-06 18:47:47 -0400

    connected to spotify

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,24 @@
 "use client"
 import { Button } from "@heroui/button";
 import { useState } from "react";
+import { signIn, useSession } from "next-auth/react";
 
 export default function Home() {
   const [login, setLogin] = useState(false); // <-- use state
+  const { data: session } = useSession();
+
+  const loginToSpotify = () => {
+    signIn("spotify"); // this will redirect to Spotify login
+    setLogin(!login)
+  }
 
   return (
     <div className="min-h-screen flex flex-col justify-between">
-      {/* Centered login content */}
       <div className="flex-grow flex flex-col items-center justify-center">
         <div className="p-4 text-xl">Login to Spotify</div>
         <div>
-          <Button onPress={() => setLogin(!login)} // <-- toggle state
+          <Button
+            onPress={() => signIn("spotify")}
             variant="flat"
             radius="md"
           >
@@ -20,14 +27,13 @@ export default function Home() {
         </div>
       </div>
 
-      {/* Status pinned to the bottom */}
       <div className="p-4 text-xs text-center font-extralight">
-        {login ? (
+        {session ? (
           <div className="text-green-500">logged in.</div>
         ) : (
           <div className="text-red-500">not logged in.</div>
         )}
       </div>
-    </div >
+    </div>
   );
 }
